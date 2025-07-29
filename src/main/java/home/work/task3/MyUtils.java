44ea3d563991a81da08c0ebc4bdb1cc63b25d0c9
Demyd <demidd06@gmail.com>
2025-07-29 21:29:20 +0300

    Initial default configuration

---
+++ src/main/java/home/work/task3/MyUtils.java
@@ -0,0 +1,15 @@
+package home.work.task3;
+
+import java.util.*;
+
+public class MyUtils {
+    // Code
+    public boolean listMapCompare(List<String> list, Map<String, String> map) {
+        // Code
+        //
+
+        return false; // TODO Update
+        //
+    }
+
+}
